a53ca5c6af2dff219664f1976fa5ae8efb7f1849
Vanessa <th3.van89@gmail.com>
2026-01-22 11:34:05 +0000

    testing compile errors

--- Core/Inc/game_logic.h
+++ Core/Inc/game_logic.h
@@ -1,17 +1,27 @@
 #ifndef INC_GAME_LOGIC
 #define INC_GAME_LOGIC
 
-
 #include <cstdint>
 
-extern "C" 
+#ifndef GAME_LOGIC_H
+#define GAME_LOGIC_H
+
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void frame_start(uint8_t frame[128][8], uint8_t player_input_x,
+                 uint8_t player_input_y, double delta_time);
 void game_init();
 
-extern "C" 
-void frame_start(uint8_t frame[128][8], uint8_t player_input_x, uint8_t player_input_y, double delta_time);
-
+#ifdef __cplusplus
+}
+#endif
 void delta_time();
 
 uint8_t get_inputs(uint8_t axis);
 
 #endif /* INC_GAME_LOGIC_H_ */
+
+#endif /* GAME_LOGIC_H */
